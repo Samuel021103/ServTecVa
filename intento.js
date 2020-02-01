f166f61cd79f288e7a4a8eae961fdfa7973bdb15
Samuel021103 <Samuel021103@gmail.com>
2020-01-31 19:48:03 -0600

    0.0.1

---
+++ intento.js
@@ -0,0 +1,15 @@
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let i = 0;
+let botton =document.getElementById("boton");
+
+boton.addEventListener("click", function(){
+    if(i==0){
+        enlaces.className = ("enlaces dos")
+        i = 1;
+    }else{
+        enlaces.classList.remove("dos")
+        enlaces.className = ("enlaces uno")
+        i = 0;
+    }
+})
